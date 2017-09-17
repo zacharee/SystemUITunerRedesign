12c1a7e692468ba819b350849907c0261ee9e140
dimqua <dimqua@riseup.net>
2017-09-17 19:50:22 +0000

    Introduce F-Droid build flavor

---
+++ app/src/fdroid/java/com/zacharee1/systemuituner/activites/MainActivity.java
@@ -0,0 +1,85 @@
+package com.zacharee1.systemuituner.activites;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.zacharee1.systemuituner.R;
+import com.zacharee1.systemuituner.misc.BillingUtil;
+import com.zacharee1.systemuituner.misc.OptionSelected;
+import com.zacharee1.systemuituner.misc.RecreateHandler;
+
+@SuppressWarnings("unused")
+public class MainActivity extends AppCompatActivity
+{
+    @SuppressWarnings("FieldCanBeLocal")
+    private static boolean DARK = false;
+    private BillingUtil mBilling;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        DARK = sharedPreferences.getBoolean("dark_mode", false);
+
+        setTheme(DARK ? R.style.AppTheme_Dark : R.style.AppTheme);
+
+        RecreateHandler.onCreate(this);
+
+        setContentView(R.layout.activity_main);
+
+        mBilling = new BillingUtil(this);
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        RecreateHandler.onDestroy(this);
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        return OptionSelected.doAction(item, this);
+    }
+
+    public void launchList(@Nullable View v) {
+        startActivity(new Intent(this, ItemListActivity.class));
+    }
+
+    public void onDonatePayPalClicked(View v) {
+        BillingUtil.onDonatePayPalClicked(this);
+    }
+
+    public void onDonate1Clicked(View v) {
+        mBilling.onDonateClicked("donate_1");
+    }
+
+    public void onDonate2Clicked(View v) {
+        mBilling.onDonateClicked("donate_2");
+    }
+
+    public void onDonate5Clicked(View v) {
+        mBilling.onDonateClicked("donate_5");
+    }
+
+    public void onDonate10Clicked(View v) {
+        mBilling.onDonateClicked("donate_10");
+    }
+}
