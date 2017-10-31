2e32d2b9814778907492cdd19543eac211215768
Zachary <zachary.wander@gmail.com>
2017-10-31 07:37:51 -0400

    Add QS grid settings for TouchWiz

--- app/src/main/java/com/zacharee1/systemuituner/fragmenthelpers/TWHelper.java
+++ app/src/main/java/com/zacharee1/systemuituner/fragmenthelpers/TWHelper.java
@@ -6,6 +6,7 @@ import android.provider.Settings;
 
 import com.zacharee1.systemuituner.fragments.ItemDetailFragment;
 import com.zacharee1.systemuituner.misc.SettingsUtils;
+import com.zacharee1.systemuituner.prefs.SliderPreferenceEmbedded;
 
 public class TWHelper extends BaseHelper
 {
@@ -14,41 +15,68 @@ public class TWHelper extends BaseHelper
     public TWHelper(ItemDetailFragment fragment) {
         mFragment = fragment;
 
-        setHBWState();
-        setHBWListener();
+//        setHBWState();
+//        setHBWListener();
+        setUpQSStuff();
     }
 
-    private void setHBWState() {
-        String hbw = Settings.Global.getString(mFragment.getContext().getContentResolver(), "limit_brightness_state");
-        SwitchPreference hbwSwitch = (SwitchPreference) mFragment.findPreference("high_bright_warning");
+    private void setUpQSStuff() {
+        SliderPreferenceEmbedded rows = (SliderPreferenceEmbedded) mFragment.findPreference("qs_tile_row");
+        SliderPreferenceEmbedded columns = (SliderPreferenceEmbedded) mFragment.findPreference("qs_tile_column");
+        int defVal = 3;
+        int savedRowVal = Settings.Secure.getInt(mFragment.getActivity().getContentResolver(), rows.getKey(), defVal);
+        int savedColVal = Settings.Secure.getInt(mFragment.getActivity().getContentResolver(), columns.getKey(), defVal);
 
-        if (hbw == null || hbw.isEmpty()) {
-            hbwSwitch.setChecked(false);
-        } else {
-            hbwSwitch.setChecked(true);
-        }
+        rows.setProgressState(savedRowVal);
+        columns.setProgressState(savedColVal);
 
-        hbwSwitch.setChecked(false);
-        hbwSwitch.setEnabled(false);
-    }
-
-    private void setHBWListener() {
-        SwitchPreference hbwSwitch = (SwitchPreference) mFragment.findPreference("high_bright_warning");
-
-        hbwSwitch.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener()
-        {
+        rows.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
-            public boolean onPreferenceChange(Preference preference, Object o)
-            {
-                if (Boolean.valueOf(o.toString())) {
-                    SettingsUtils.writeGlobal(mFragment.getContext(), "limit_brightness_state", "80,80");
-                } else {
-                    SettingsUtils.writeGlobal(mFragment.getContext(), "limit_brightness_state", "");
-                }
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                SettingsUtils.writeSecure(mFragment.getActivity(), preference.getKey(), newValue.toString());
+                return true;
+            }
+        });
+        columns.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                SettingsUtils.writeSecure(mFragment.getActivity(), preference.getKey(), newValue.toString());
                 return true;
             }
         });
     }
+
+//    private void setHBWState() {
+//        String hbw = Settings.Global.getString(mFragment.getContext().getContentResolver(), "limit_brightness_state");
+//        SwitchPreference hbwSwitch = (SwitchPreference) mFragment.findPreference("high_bright_warning");
+//
+//        if (hbw == null || hbw.isEmpty()) {
+//            hbwSwitch.setChecked(false);
+//        } else {
+//            hbwSwitch.setChecked(true);
+//        }
+//
+//        hbwSwitch.setChecked(false);
+//        hbwSwitch.setEnabled(false);
+//    }
+
+//    private void setHBWListener() {
+//        SwitchPreference hbwSwitch = (SwitchPreference) mFragment.findPreference("high_bright_warning");
+//
+//        hbwSwitch.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener()
+//        {
+//            @Override
+//            public boolean onPreferenceChange(Preference preference, Object o)
+//            {
+//                if (Boolean.valueOf(o.toString())) {
+//                    SettingsUtils.writeGlobal(mFragment.getContext(), "limit_brightness_state", "80,80");
+//                } else {
+//                    SettingsUtils.writeGlobal(mFragment.getContext(), "limit_brightness_state", "");
+//                }
+//                return true;
+//            }
+//        });
+//    }
 
     @Override
     public void onDestroy() {
